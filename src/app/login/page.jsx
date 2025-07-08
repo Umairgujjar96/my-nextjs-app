c1b7caaf3f57f3c5d1e1e25696a083c4646c44aa
Umairgujjar96 <umairhassn83937@gmail.com>
2025-07-08 17:04:06 +0500

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,7 +2,7 @@ import Login from '@/components/Auth/login'
 import React from 'react'
 
 const page = () => {
-  console.log("HI")
+  console.log("HI World")
   return (
     <div>
         <Login />
